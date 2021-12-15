51fa49737be2efebf7af8a4a631eb72fb8555896
YC-20210222AEXP\Administrator <zjp65718732@163.com>
2021-12-15 11:12:00 +0800

    chore:create vue3.0

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # vue-learn
-练习vue3.0 + ts 
+vue3.0 项目构建测试
+
+## Project setup
+```
+pnpm install
+```
+
+### Compiles and hot-reloads for development
+```
+pnpm run serve
+```
+
+### Compiles and minifies for production
+```
+pnpm run build
+```
+
+### Lints and fixes files
+```
+pnpm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+import { defineAsyncComponent } from "vue";
+
+export default {
+  name: "Home",
+  components: {
+    HelloWorld: defineAsyncComponent(() =>
+      import("../components/HelloWorld.vue")
+    ),
+  },
+
+  created() {
+    const num = 123;
+
+    console.log(num === 123);
+  },
+};
+</script>
